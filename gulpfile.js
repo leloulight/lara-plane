bc5c4370359eeb32323a832975407b06ab891594
lollipop.fly@gmail.com <lollipop.fly@gmail.com>
2015-09-24 23:20:09 +0300

    make new gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,66 @@
-var elixir = require('laravel-elixir');
+var gulp = require('gulp'),
+	// csswring = require('csswring'),
+	postcss = require('gulp-postcss'),
+	sass = require('gulp-sass'),
+	autoprefixer = require('autoprefixer'),
+	spritesmith = require('gulp.spritesmith'),
+	rucksack = require('gulp-rucksack'),
+	browserSync = require('browser-sync').create(),
+	merge = require('merge-stream');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
+// Sass
+gulp.task('sass', function() {
+	var processors = [
+	       // csswring,
+	       autoprefixer({ browsers: ['last 20 versions'] }),
+	       require('postcss-font-magician')({}),
+	   ];
 
-elixir(function(mix) {
-    mix.sass('app.scss');
+	return gulp.src('sass/style.scss')
+		.pipe(sass())
+		.pipe(postcss(processors))
+		.pipe(gulp.dest('css/'));
 });
+
+// Sprites
+gulp.task('sprite', function () {
+  var spriteData = gulp.src('images/main/*.png').pipe(spritesmith({
+    imgName: 'sprite.png',
+    cssName: 'sprite.css',
+    padding: 1,
+  }));
+
+  var imgStream = spriteData.img
+     .pipe(gulp.dest('images/'));
+
+   var cssStream = spriteData.css
+     .pipe(gulp.dest('css/'));
+
+   return merge(imgStream, cssStream);
+  // return spriteData.pipe(gulp.dest('css/'));
+});
+
+
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: ""
+    });
+
+    // gulp.watch("/sass/*.scss", { interval: 500 }, ['sass']);
+    gulp.watch("css/style.css").on('change', browserSync.reload);
+    gulp.watch("index.html").on('change', browserSync.reload);
+});
+
+// Watch
+gulp.task('watch', function() {
+	gulp.watch('sass/style.scss', { interval: 500 }, ['sass']);
+	gulp.watch('images/main/*.png', { interval: 500 }, ['sprite']);
+});
+
+// Default task
+gulp.task('default', ['sass', 'sprite', 'watch']);
+
+
+
+
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,10 +1,45 @@
-<!DOCTYPE html>
-<html lang="ru">
+<!doctype html>
+<html>
 <head>
-	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Sass Template</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- BOOTSTRAP -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+
+	<!-- BXSLIDER -->
+	<link rel="stylesheet" href="css/jquery.bxslider.css">
+
+	<!-- FONT-AWESOME -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+
+	<link rel="stylesheet" href="css/style.css" >
+
+	<!--[if lt IE 9]>
+		<script src="js/libs/html5shiv.min.js"></script>
+	<![endif]-->
 </head>
 <body>
-	@yield('content')
+
+	<!-- begin wrap  -->
+	<div class="wrap container">
+
+		@yield('content');
+
+	</div>
+	<!-- end wrap -->
+
+	<footer class="footer">
+
+	</footer>
+
+	<!-- SCRIPTS -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script src='js/libs/bootstrap.min.js'></script>
+	<script src='js/libs/jquery.bxslider.min.js'></script>
+	<script src='js/common.js'></script>
+	<!-- <script src="js/build/global.min.js"></script> -->
 </body>
 </html>
