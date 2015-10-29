ef03978b37e16b9950aa44340a7d50515e2b24c8
lollipop.fly@gmail.com <lollipop.fly@gmail.com>
2015-10-29 21:46:34 +0300

    delete old preview image after update

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -7,19 +7,18 @@ use App\Http\Requests\SpaceshipRequest; // for validation
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use File; // for file deleting
+use Hash; // for random string generate for preview image name
 use Illuminate\Support\Facades\Request;
 
 class adminController extends Controller
 {
     public function index() {
         $spaceships = Spaceships::latest('created_at')->paginate(5);
-
         return view('admin.index', compact('spaceships'));
     }
 
     public function show($id) {
         $spaceship = Spaceships::findOrFail($id);
-
         return view('admin.show', compact('spaceship'));
     }
 
@@ -31,27 +30,16 @@ class adminController extends Controller
     public function store(SpaceshipRequest $request) {
         $destinationPath = 'uploads/spaceships'; // uploads folder
 
-        $spaceships = new Spaceships(Array(
-            'name' => $request->get('name'),
-            'assignment' => $request->get('assignment'),
-            'real' => $request->get('real'),
-            'description' => $request->get('description'),
-            'meta-description' => $request->get('meta-description'),
-            'country' => $request->get('country'),
-            'video' => $request->get('video'),
-            'cost' => $request->get('cost'),
-            'crew' => $request->get('crew'),
-            'speed' => $request->get('speed'),
-            'length' => $request->get('length'),
-            'width' => $request->get('width'),
-            'height' => $request->get('height'),
-        ));
+        $data = $request->all();
+        $spaceships = new Spaceships($data);
 
         // If has preview image
         if($request->hasFile('preview')) {
-
             $preview = $request->file('preview');
             $previewName = $preview->getClientOriginalName();
+
+            // generate hash for uniq image name
+            $previewName = $this->generateNameForPreview($previewName);
 
             $request->file('preview')->move($destinationPath, $previewName);
             // add to model
@@ -83,8 +71,14 @@ class adminController extends Controller
 
         // If has preview image
         if($request->hasFile('preview')) {
+            // delete old preview image
+            File::delete($flight->preview);
+
             $preview = $request->file('preview');
             $previewName = $preview->getClientOriginalName();
+
+            // generate new unique name for preview image
+            $previewName = $this->generateNameForPreview($previewName);
 
             $request->file('preview')->move($destinationPath, $previewName);
             $previewName = $destinationPath . '/' . $previewName;
@@ -101,10 +95,20 @@ class adminController extends Controller
         $flight = Spaceships::find($id);
 
         File::delete($flight->preview); // delete preview image
-
         $flight->delete();
 
         session()->flash('flash_message', 'Корабль ' . $flight->name . ' был удален!');
         return redirect('admin');
     }
+
+
+    // generate new unique name for preview image
+    public function generateNameForPreview($previewName) {
+        // generate hash for uniq image name
+        $hash = Hash::make('secret');
+        $hash = substr($hash, 0, 4);
+        $previewName = $hash . '_' . $previewName;
+        return $previewName;
+    }
+
 }
